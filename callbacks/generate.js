656798c303235b376101079c6d9b10fc863db64e
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-07-18 14:02:04 +0300

    реализованна гереция

--- callbacks/generate.js
+++ callbacks/generate.js
@@ -1,26 +1,25 @@
-const fs = require('fs');
-const mustache = require('mustache');
-
-const data = {};
-
-fs.readFile('data.json', {encoding: 'utf-8'}, (err, res) => {
-    if (err) {
-        console.log(err);
-    } else {
-        data.course = JSON.parse(res).course;
-
-        fs.readFile('template.html', {encoding: 'utf-8'}, (err, res) => {
-            if (err) {
-                console.log(err);
-            } else {
-                data.template = res;
-
-                mustache.parse(data.template);
+const
+    fs = require('fs'),
+    mustache = require('mustache');
 
 
-                console.log(mustache.render(data.template, data.course))
-            }
+// чтение данных
+fs.readFile('data.json', {encoding: 'utf-8'}, (err, data) => {
+    if (err) throw err;
+
+    const view = JSON.parse(data);
+    // чтение шаблона
+    fs.readFile('template.html', {encoding: 'utf-8'}, (err, template) => {
+        if (err) throw err
+
+        // необязательно, ускоряет последующее использование
+        mustache.parse(template);
+        // генерация выходного html
+        const build = mustache.render(template, view);
+        // запись сгенерированного файла
+        fs.writeFile('build.html', build, err => {
+            if (err) throw err;
         });
-    }
+    });
 });
 
