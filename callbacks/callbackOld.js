a753fee0ae62cf5bdb6b01d49e61a3ab920d5e3d
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-07-19 13:55:33 +0300

    лапша коллбэков

---
+++ callbacks/callbackOld.js
@@ -0,0 +1,24 @@
+const
+    fs = require('fs'),
+    mustache = require('mustache');
+
+
+// чтение данных
+fs.readFile('data.json', {encoding: 'utf-8'}, (err, data) => {
+    if (err) throw err;
+
+    const view = JSON.parse(data);
+    // чтение шаблона
+    fs.readFile('template.html', {encoding: 'utf-8'}, (err, template) => {
+        if (err) throw err
+
+        // необязательно, ускоряет последующее использование
+        mustache.parse(template);
+        // генерация выходного html
+        const build = mustache.render(template, view);
+        // запись сгенерированного файла
+        fs.writeFile('build.html', build, err => {
+            if (err) throw err;
+        });
+    });
+});
