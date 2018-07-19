5f4bdbd37a755c117af99694941b3fbea668fe84
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-07-19 15:22:36 +0300

    refactoring

--- callbacks/promise.js
+++ callbacks/promise.js
@@ -3,28 +3,28 @@ const
     mustache = require('mustache');
 
 
-new Promise((resole, reject) => {
+new Promise((resolve, reject) => {
     fs.readFile('data.json', {encoding: 'utf-8'}, (err, data) => {
         if (err) reject(err);
-        else resole(JSON.parse(data));
+        else resolve(JSON.parse(data));
     });
 })
 .then(view => {
-    return new Promise((resole, reject) => {
+    return new Promise((resolve, reject) => {
         fs.readFile('template.html', {encoding: 'utf-8'}, (err, template) => {
             if (err) reject(err);
             else {
                 // необязательно, ускоряет последующее использование
                 mustache.parse(template);
                 // генерация выходного html
-                resole(mustache.render(template, view));
+                resolve(mustache.render(template, view));
             };
         });
     });
 })
 .then(build => {
     // запись сгенерированного файла
-    return new Promise((resole, reject) => {
+    return new Promise((resolve, reject) => {
         fs.writeFile('build.html', build, err => {
             if (err ) reject(err);
         });
