9e24f8c71a2d4fc60311414eee880bfb3f65d4dc
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-07-19 13:55:13 +0300

    реализация через async.map

---
+++ callbacks/callback2.js
@@ -0,0 +1,35 @@
+const
+    fs = require('fs'),
+    mustache = require('mustache'),
+    async = require('async');
+
+
+async.map(
+    // --------DATA---------
+    ['data.json', 'data2.json'],
+    // ------FUNCTION------
+    (file, cb) => {
+        fs.readFile(file, {encoding: 'utf-8'}, (err, data) => {
+            data = JSON.parse(data);
+            cb(err, data);
+        })
+    },
+    // ------CALLBACK------
+    (err, view) => {
+    if (!err) {
+        view = Object.assign(...view);
+        fs.readFile('template.html', {encoding: 'utf-8'}, (err, template) => {
+            if (err) throw err
+            // необязательно, ускоряет последующее использование
+            mustache.parse(template);
+            // генерация выходного html
+            const build = mustache.render(template, view);
+            // запись сгенерированного файла
+            fs.writeFile('build.html', build, err => {
+                if (err) throw err;
+            });
+        });
+    } else {
+        console.log('Error:', err)
+    }
+});
