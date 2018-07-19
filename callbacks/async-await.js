fb62d33f172a7a80ce8bcb8c3575c2fb5e740f5b
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-07-19 15:52:07 +0300

    developed with async await

--- callbacks/async-await.js
+++ callbacks/async-await.js
@@ -1,42 +1,25 @@
 const
-    fs = require('fs'),
+    Promise = require('bluebird'),
+    fs = Promise.promisifyAll(require('fs')),
     mustache = require('mustache');
 
 
-async function generateHTML() {
-    let data;
-    await fs.readFile('data.json', {encoding: 'utf-8'}, (err, res) => {
-        if (err) throw err;
-        data = JSON.parse(res);
-    });
-
-
+async function generateHTML(files) {
+    const data = [];
+    for (let file of files) {
+        let temp =  await fs.readFileSync(file, 'utf-8');
+        data.push(JSON.parse(temp));
+    }
+    const
+        view = Object.assign(...data);
+        template = await fs.readFileSync('template.html', 'utf-8');
+        build = mustache.render(template, view);
+    await fs.writeFileSync('build.html', build);
 }
-new Promise((resole, reject) => {
-    fs.readFile('data.json', {encoding: 'utf-8'}, (err, data) => {
-        if (err) reject(err);
-        else resole(JSON.parse(data));
-    });
-})
-.then(view => {
-    return new Promise((resole, reject) => {
-        fs.readFile('template.html', {encoding: 'utf-8'}, (err, template) => {
-            if (err) reject(err);
-            else {
-                // необязательно, ускоряет последующее использование
-                mustache.parse(template);
-                // генерация выходного html
-                resole(mustache.render(template, view));
-            };
-        });
-    });
-})
-.then(build => {
-    // запись сгенерированного файла
-    fs.writeFile('build.html', build, err => {
-        if (err) throw err;
-    });
-})
-.catch(err => console.log(err));
 
-generateHTML()
+const files = ['data.json', 'data2.json'];
+try {
+    generateHTML(files);
+} catch (err) {
+    console.log(err);
+}
