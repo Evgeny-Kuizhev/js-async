592afe1e7746ca5adc29598bfa6e6a87a484bdbe
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-07-20 11:22:55 +0300

    refactoring

---
+++ callbacks/callbacks2.js
@@ -0,0 +1,34 @@
+const
+    fs = require('fs'),
+    mustache = require('mustache'),
+    async = require('async');
+
+
+async.map(
+    // --------DATA---------
+    ['data.json', 'data2.json'],
+    // ------FUNCTION------
+    (file, cb) => {
+        fs.readFile(file, {encoding: 'utf-8'}, (err, data) => {
+            if (err) throw err;
+            data = JSON.parse(data);
+            cb(err, data);
+        });
+    },
+    // ------CALLBACK------
+    (err, result) => {
+    if (err) throw err;
+
+    const view = Object.assign(...result);
+    fs.readFile('template.html', {encoding: 'utf-8'}, (err, template) => {
+        if (err) throw err
+        // необязательно, ускоряет последующее использование
+        mustache.parse(template);
+        // генерация выходного html
+        const build = mustache.render(template, view);
+        // запись сгенерированного файла
+        fs.writeFile('build.html', build, err => {
+            if (err) throw err;
+        });
+    });
+});
