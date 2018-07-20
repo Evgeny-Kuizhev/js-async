99ea7c92aa37f76f1e0002cb0bf6800fbaee0a69
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-07-20 16:59:34 +0300

    разработан tail node js

---
+++ streams/tail.js
@@ -0,0 +1,21 @@
+const
+    fs = require('fs'),
+
+    fileName = process.argv[2];
+    n = +process.argv[3];
+    file = fs.createReadStream(fileName, 'utf8');
+
+let lastItems = [];
+file.on('data', chunk => {
+    let lines = chunk.split('\n');
+    if (lines.length >= n) {
+        lastItems = lines.slice(-n);
+    } else {
+        lastItems.push(lines);
+        lastItems = lastItems.slice(-n);
+    }
+})
+
+file.on('end', () => {
+    lastItems.forEach(el => process.stdout.write(`${el}\n`));
+})
